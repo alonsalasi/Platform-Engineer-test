2a5909972429cf9d1541f00167234794cdc1c914
alonsalasi <alon.salasi@leidos.com>
2025-11-23 16:15:04 +0200

    Add S3 backend for terraform state

--- provider.tf
+++ provider.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.0"
   
+  backend "s3" {
+    bucket = "terraform-state-ecs-platform-alonsalasi"
+    key    = "ecs-clusters/terraform.tfstate"
+    region = "us-east-1"
+  }
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
