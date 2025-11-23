a27b8ef7e7a23caecaf13fcafbdb3bd425982bad
alonsalasi <alon.salasi@leidos.com>
2025-11-23 18:18:15 +0200

    Fix delete cluster sed pattern with awk

--- ecs-clusters.tf
+++ ecs-clusters.tf
@@ -1,11 +1,4 @@
 # ECS Clusters managed by K8s Platform
 # This file is auto-updated by GitHub Actions
+
 # Clusters will appear here after creation
-  
-  tags = {
-    Name = "test2"
-    ManagedBy = "K8s-GitOps"
-    CreatedBy = "k8s-platform"
-  }
-}
-}
