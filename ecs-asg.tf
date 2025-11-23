067290c949fe48398ffec4ff4e9d3ffcaa7dd54e
alonsalasi <alon.salasi@leidos.com>
2025-11-23 19:06:31 +0200

    Clean broken ASG file

--- ecs-asg.tf
+++ ecs-asg.tf
@@ -2,36 +2,3 @@
 # This file is auto-updated by GitHub Actions
 
 # ASGs will appear here after creation
-
-}
-
-  
-  user_data = base64encode(<<-EOF
-    #!/bin/bash
-    echo ECS_CLUSTER=temp-cluster >> /etc/ecs/ecs.config
-  EOF
-  )
-  
-  tag_specifications {
-    resource_type = "instance"
-    tags = {
-      Name = "test-instance"
-      ManagedBy = "K8s-GitOps"
-    }
-  }
-}
-
-  
-  tag {
-    key                 = "Name"
-    value               = "test"
-    propagate_at_launch = true
-  }
-  
-  tag {
-    key                 = "AmazonECSManaged"
-    value               = "true"
-    propagate_at_launch = true
-  }
-}
-
